12670b8f4b0531c4039b962cb6f3366fe63fea2a
rcook0 <141237640+rcook0@users.noreply.github.com>
2025-09-16 14:11:15 +0200

    Create playwright.config.js

---
+++ Express+MySQL/playwright.config.js
@@ -0,0 +1,11 @@
+// @ts-check
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  use: {
+    headless: true,
+    baseURL: 'http://localhost:5173',
+    trace: 'on-first-retry',
+  },
+});
