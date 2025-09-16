5bc464c7bdb2059bfabc58752b5b5dad12c9f4d9
rcook0 <141237640+rcook0@users.noreply.github.com>
2025-09-16 14:32:21 +0200

    Create server.js

---
+++ Express+MySQL/backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import airlinesRouter from "./routes/airlines.js";
+import flightsRouter from "./routes/flights.js";
+import reservationsRouter from "./routes/reservations.js";
+
+const app = express();
+app.use(bodyParser.json());
+app.use(morgan("dev"));
+
+app.get("/", (_req, res) => res.send("Airline Reservation API up"));
+
+app.use("/api/airlines", airlinesRouter);
+app.use("/api/flights", flightsRouter);
+app.use("/api/reservations", reservationsRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(
+    `Server running on http://localhost:${PORT} (ORM=${process.env.ORM || "prisma"})`
+  );
+});
