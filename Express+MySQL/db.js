59fcbcf5233a63fc029a91e6aae64ca0f03d105d
rcook0 <141237640+rcook0@users.noreply.github.com>
2025-09-16 14:04:21 +0200

    Update db.js

--- Express+MySQL/db.js
+++ Express+MySQL/db.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "airline_db",
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "password",
+  database: process.env.DB_NAME || "airline_db",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
