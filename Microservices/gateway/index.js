4fed6b33d1bb3a3e254606fdc943a1ca3dce8e7e
rcook0 <141237640+rcook0@users.noreply.github.com>
2025-10-06 16:11:21 +0200

    Delete Microservices directory

--- Microservices/gateway/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import proxy from "express-http-proxy";
-
-const app = express();
-const bookingUrl = process.env.BOOKING_URL || "http://localhost:4001";
-
-app.use("/api/reservations", proxy(bookingUrl));
-
-app.listen(3000, () => console.log("API Gateway on :3000"));
